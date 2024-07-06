7cf7422b0940fb6d77649c0098663f40e0bee46c
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-07-06 20:40:47 +0530

    rest code 2

---
+++ reactrevise2/src/components/ProductDate.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ProductDate.css";
+
+const ProductDate = (props) => {
+  const month = props.date.toLocalString("en-us", { month: "long" });
+  const day = props.date.toLocalString("en-us", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div className="product-date">
+      <div className="product-date__month">{month}</div>
+      <div className="product-date__year">{year}</div>
+      <div className="product-date__day">{day}</div>
+    </div>
+  );
+};
+
+export default ProductDate;
